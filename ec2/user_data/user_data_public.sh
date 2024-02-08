f1ce9e8c5e3186986d1b8a28c62a6e5b4286e20f
JAESUNGPARK <batson1@naver.com>
2024-02-08 16:02:02 +0900

    user data public

--- ec2/user_data/user_data_public.sh
+++ ec2/user_data/user_data_public.sh
@@ -1,11 +1,8 @@
-export MASTER_USER=jsp
-export MASTER_USER_PASSWORD=sjaksahffk.!
-
-adduser $MASTER_USER -g wheel
-echo $MASTER_USER:$MASTER_USER_PASSWORD | chpasswd
-
-rsync -a /etc/ssh/sshd_config /root/HOSTWAY/
 sed -i 's/#PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+
+sed -i 's/^.*10" //g' /root/ssh/authorized_keys
+
+echo "P@ssw0rd!12#" | passwd root --stdin
 
 systemctl restart sshd
